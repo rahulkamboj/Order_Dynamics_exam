b9b6e7bf913bd390ca3286d714f5df7af5c30e12
AbhiThakare <thakareabhinav@gmial.com>
2014-12-05 19:06:31 +0530

    Test

---
+++ contact.php
@@ -0,0 +1,25 @@
+// Contact.PHP
+<?php
+echo "<BR>";
+echo $organization; 
+echo "<BR>";
+echo $name;
+echo "<BR>";
+echo $title; 
+echo "<BR>";
+echo $homeadda;
+echo "<BR>";
+echo $city; 
+echo "<BR>";
+echo $state;
+echo "<BR>";
+echo $zip; 
+echo "<BR>";
+echo $phone;
+echo "<BR>";
+echo $fax; 
+echo "<BR>";
+echo $emailadda;
+echo "<BR>";
+echo $comments;
+?>
